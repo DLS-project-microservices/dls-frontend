9784525a5be7a44f852883695b6cefa504f99ace
RenasAli <rena0506@stud.kea.dk>
2024-03-25 12:20:29 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './root.css';
 import './index.css';
 import { Container, Header} from './sections/index.js'
 import { Home, OmOs, Kontakt } from './pages/index.js'
